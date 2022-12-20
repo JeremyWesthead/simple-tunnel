6cdb8d7f68630e74cdb66d9cf349507a738e1178
JeremyWesthead <jeremywesthead3@gmail.com>
2022-12-20 11:36:20 +0000

    Various improvements

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # simple-tunnel
-Simple SSH tunnel manager
+Simple SSH tunnel manager. Utilises systemd user services to create daemon processes. Useful for accessing remote kubernetes clusters via Lens
+
+## Requirements
+* Systemd
+* SSH
+* Python3
+
+## Usage
+### Create new tunnel
+Create a tunnel from 10.8.12.4:80 to localhost:6000
+```
+python simple-tunnel.py --name test-tunnel --local_port 6000 --ext_ip 10.8.12.4 --ext_port 80
+```
+
+Create a tunnel from 10.8.12.4:80 to localhost:6000 which jumps via some.proxy.com
+```
+python simple-tunnel.py --name test-tunnel --local_port 6000 --ext_ip 10.8.12.4 --ext_port 80 --proxy some.proxy.com
+```
+
+### List running tunnel services
+```
+python simple-tunnel.py --list
+```
+
+## Possible future additions
+* Check for overwriting
+* Added error message propagation
+* Add management of tunnels e.g deletion
+* GUI
+    * Tray icon
+    * Notifications on fail
+
+## Current limitations
+* Only supports key-based access
+
--- simple-tunnel.py
+++ simple-tunnel.py
@@ -3,30 +3,53 @@
 import argparse
 import os
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ext_ip", required=True, help="External IP address")
-    parser.add_argument("--ext_port", required=True, help="External port")
-    parser.add_argument("--local_port", required=True, help="Local port")
-    parser.add_argument("--name", required=True, help="Name of the connection")
-    parser.add_argument("--proxy", required=False, default="", help="A proxy jump")
-
-    options = parser.parse_args()
+def create(options):
+    '''Create a systemd user service to run the specified SSH tunnel
+    '''
+    if not (options.ext_ip and options.ext_port and options.local_port):
+        print("Missing options. --ext_ip, --ext_port and --local_port are required to create a tunnel.")
+        print("Try --help for troubleshooting.")
+        return
 
     template = f'''[Unit]
 Description=SSH tunnel for {options.name}
 
 [Service]
-ExecStart=/usr/bin/ssh -N -L localhost:{options.local_port}:{options.ext_ip}:{options.ext_port} -o ExitOnForwardFailure=yes -o ServerAliveInterval=15 -o ServerAliveCountMax=3 -p 22  {options.proxy}
+ExecStart=/usr/bin/ssh -N -L {options.local_ip}:{options.local_port}:{options.ext_ip}:{options.ext_port} -o ExitOnForwardFailure=yes -o ServerAliveInterval=15 -o ServerAliveCountMax=3 -p 22  {options.proxy}
 RestartSec=5
 Restart=always
 
 
 [Install]
 WantedBy=default.target'''
+
     with open(os.path.expanduser(f"~/.config/systemd/user/simple-tunnel-{options.name}.service"), "w") as f:
         f.write(template)
     
-    os.system(f"systemctl --user start simple-tunnel-{options.name}")
+    os.system(f"systemctl --user {'enable --now' if options.persist else 'start'} simple-tunnel-{options.name}")
+
+
+def list_tunnels():
+    '''Print the names and statuses of the simple-tunnel services
+    '''
+    os.system("systemctl --user list-units | grep simple-tunnel | cut -d' ' -f3")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ext_ip", default=None, help="External IP address")
+    parser.add_argument("--ext_port", default=None, help="External port")
+    parser.add_argument("--local_ip", default="localhost", help="Local IP")
+    parser.add_argument("--local_port", default=None, help="Local port")
+    parser.add_argument("--name", default=None, help="Name of the connection")
+    parser.add_argument("--proxy", default="", help="A proxy jump")
+    parser.add_argument("--list", action='store_true', default=False, help="Show the current services")
+    parser.add_argument("--persist", action='store_true', default=False, help="Persist the service between reboots")
+
+    options = parser.parse_args()
+
+    if options.list:
+        list_tunnels()
+    else:
+        create(options)
     
 
